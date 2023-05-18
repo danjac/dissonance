9a9dd225e519defdb044a65618dd7ec1a1ebb985
Dan Jacob <danjac2018@gmail.com>
2023-05-18 17:20:53 +0300

    feat: basic chat working

--- dissonance/chatrooms/views.py
+++ dissonance/chatrooms/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 from django.db import connection
 from django.http import Http404, HttpRequest, HttpResponse, StreamingHttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from django.template.loader import render_to_string
 from django.views.decorators.http import require_POST
 
 from dissonance.chatrooms.forms import RoomForm
@@ -23,7 +22,7 @@ def index(request: HttpRequest) -> HttpResponse:
 def room_detail(request: HttpRequest, room_id: int) -> HttpResponse:
     room = get_object_or_404(Room.objects.select_related("owner"), pk=room_id)
     messages = (
-        Message.objects.filter(room=room).select_related("user").order_by("-created")
+        Message.objects.filter(room=room).select_related("user").order_by("created")
     )
 
     return render(
@@ -61,68 +60,64 @@ def post_message(request: HttpRequest, room_id: int) -> HttpResponse:
         _notify(
             channel=f"room{room.pk}",
             event="new-message",
-            event_id=str(message.pk),
-            data=_render_message(message),
+            data=str(message.pk),
         )
-    return HttpResponse("OK")
+    return render(request, "chatrooms/_message_form.html", {"room": room})
+
+
+def messages(request: HttpRequest, room_id: int) -> HttpResponse:
+    room = get_object_or_404(Room, pk=room_id)
+    messages = (
+        Message.objects.filter(room=room).select_related("user").order_by("created")
+    )
+    return render(
+        request,
+        "chatrooms/_messages.html",
+        {
+            "messages": messages,
+            "room": room,
+        },
+    )
 
 
 async def messages_stream(
     request: HttpRequest,
     room_id: int,
 ) -> StreamingHttpResponse:
-    if room := Room.objects.filter(pk=room_id).afirst():
-        latest_message = Message.objects.filter(room=room).order_by("-created").afirst()
+    room = await Room.objects.filter(pk=room_id).afirst()
 
+    async def _stream_messages() -> AsyncGenerator[str, None]:
+        connection_params = connection.get_connection_params()
+        connection_params.pop("cursor_factory")
+
+        conn = await psycopg.AsyncConnection.connect(
+            **connection_params, autocommit=True
+        )
+
+        async with conn.cursor() as cursor:
+            await cursor.execute(f"LISTEN room{room.pk}")
+            gen = conn.notifies()
+            async for notify_msg in gen:
+                yield _sse_message(**json.loads(notify_msg.payload))
+
+    if room:
         return StreamingHttpResponse(
-            streaming_content=_stream_messages(room, latest_message),
+            streaming_content=_stream_messages(),
             content_type="text/event-stream",
         )
     raise Http404("Room not found")
 
 
-async def _stream_messages(
-    room: Room, latest_message: Message | None = None
-) -> AsyncGenerator[str, None]:
-    connection_params = connection.get_connection_params()
-    connection_params.pop("cursor_factory")
-
-    if latest_message:
-        yield _sse_message(
-            event="new-message",
-            event_id=str(latest_message.pk),
-            data=_render_message(latest_message),
-        )
-
-    conn = await psycopg.AsyncConnection.connect(**connection_params, autocommit=True)
-
-    async with conn.cursor() as cursor:
-        await cursor.execute(f"LISTEN room{room.pk}")
-        async for notify_msg in conn.notifies():
-            yield _sse_message(**json.loads(notify_msg.payload))
+def _sse_message(*, event: str, data: str) -> str:
+    return "\n".join([f"event: {event}", f"data: {data}", "\n"])
 
 
-def _sse_message(*event: str, event_id: str, data: str) -> str:
-    return "\n".join(
-        [
-            f"id: {event_id}",
-            f"event: {event}",
-            f"data: {data}",
-        ]
-    )
-
-
-def _notify(*, channel: str, event: str, event_id: int, data: str) -> None:
+def _notify(*, channel: str, event: str, data: str) -> None:
     payload = json.dumps(
         {
             "event": event,
-            "event_id": event_id,
             "data": data,
         },
     )
     with connection.cursor() as cursor:
         cursor.execute(f"NOTIFY {channel}, '{payload}'")
-
-
-def _render_message(message) -> str:
-    return render_to_string("chatrooms/_message.html", {"message": message})
